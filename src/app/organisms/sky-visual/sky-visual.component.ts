70ec4220b2f4a359f470c2405585347b31784804
Rob-Willson <ribrob@gmail.com>
2025-03-24 11:01:55 +0000

    add horizon glare and test house to sky-visual

--- src/app/organisms/sky-visual/sky-visual.component.ts
+++ src/app/organisms/sky-visual/sky-visual.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, ChangeDetectionStrategy, OnInit, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, ViewChild, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { select, timer } from 'd3';
 import { PointMagnitude } from '../../shared/types/point.model';
 
@@ -27,8 +27,16 @@ export class SkyVisualComponent implements OnChanges {
     public skyContainerElement!: ElementRef<HTMLElement>;
 
     private svg!: any;
+    private defs!: any;
+    private defsClipPathRect!: any;
+    private defsHorizonGlareRadialGradient!: any;
+    private horizonGlare!: any;
+    private horizonLine!: any;
+    private horizonHouse!: any;
     private starsContainer!: any;
     private stars!: any;
+
+    private readonly horizonPositionFraction: number = 0.666;
 
     public ngOnChanges(changes: SimpleChanges): void {
         if ((!changes['width'] || !changes['width'].currentValue) && (!changes['height'] || !changes['height'].currentValue)) {
@@ -48,12 +56,32 @@ export class SkyVisualComponent implements OnChanges {
         }
 
         const maxDimension = this.getMaxDimension();
+        const horizonPosition = maxDimension * this.horizonPositionFraction;
 
         this.svg
             .attr('width', maxDimension)
             .attr('height', maxDimension)
             .attr('viewBox', `0 0 ${maxDimension} ${maxDimension}`)
             .attr('preserveAspectRatio', 'xMidYMid meet');
+
+        this.defsClipPathRect
+            .attr('width', maxDimension)
+            .attr('height', horizonPosition)
+
+        this.horizonGlare
+            .attr('rx', maxDimension / 2)
+            .attr('ry', 300)
+            .attr('cx', maxDimension / 2)
+            .attr('cy', horizonPosition);
+
+        this.horizonLine
+            .attr('x1', 0)
+            .attr('y1', horizonPosition)
+            .attr('x2', maxDimension)
+            .attr('y2', horizonPosition);
+
+        this.horizonHouse
+            .attr('transform', `translate(${maxDimension / 2 - 20}, ${horizonPosition - 22})`)
 
         this.starsContainer
             .data([this.data]);
@@ -66,27 +94,88 @@ export class SkyVisualComponent implements OnChanges {
             .join('g')
                 .attr('class', 'sky-visual__container__stars-g__star-g')
                 .attr('transform', (d: PointMagnitude) => `translate (${d.x * maxDimension}, ${d.y * maxDimension})`)
+                // .style('animation-delay', () => `${Math.random() * 2 + 2}s`);
 
         this.stars.selectAll('circle')
             .data((d: any) => [d])
             .join('circle')
                 .attr('class', 'sky-visual__container__stars-g__star-g__circle')
                 .attr('r', (d: PointMagnitude) => d.magnitude)
+                .transition()
+                .delay(3000)
                 .style('animation-delay', () => `${Math.random() * 2}s`);
 
     }
 
     private generateSvg(): void {
         this.svg = select(this.skyContainerElement.nativeElement)
-            .append('svg');
+            .append('svg')
+            .attr('clip-path', 'url(#horizon)');
+
+        this.defs = this.svg
+            .append('defs');
+
+        this.defsClipPathRect = this.defs
+            .append('clipPath')
+            .attr('id', 'horizon')
+            .append('rect');
+
+        this.defsHorizonGlareRadialGradient = this.defs
+            .append('radialGradient')
+            .attr('id', 'horizon-glare')
+            .attr('cx', '50%')
+            .attr('cy', '50%')
+            .attr('r', '50%')
+            .attr('fx', '50%')
+            .attr('fy', '50%')
+            .selectAll('stop')
+            .data([
+              { offset: '0%', color: 'rgb(83, 107, 117)', opacity: 0.6 },
+              { offset: '60%', color: 'rgb(25, 53, 53)', opacity: 0.6 },
+              { offset: '100%', color: 'rgb(26, 28, 30)', opacity: 0 },
+            ])
+            .enter()
+            .append('stop')
+            .attr('offset', (d: any) => d.offset)
+            .attr('stop-color', (d: any) => d.color)
+            .attr('stop-opacity', (d: any) => d.opacity);
 
         this.starsContainer = this.svg.append('g')
             .attr('class', 'sky-visual__container__stars-g');
 
+        this.horizonGlare = this.svg
+            .append('ellipse')
+            .attr('class', 'sky-visual__container__horizon-glare');
+
+        this.horizonLine = this.svg
+            .append('line')
+            .attr('class', 'sky-visual__container__horizon-line');
+
+        this.horizonHouse = this.svg
+            .append('g')
+            .attr('class', 'sky-visual__container__horizon-house-g');
+        this.horizonHouse
+            .append('rect')
+            .attr('class', 'sky-visual__container__horizon-house-g__base')
+            .attr('width', 40)
+            .attr('height', 28);
+        this.horizonHouse
+            .append('rect')
+            .attr('class', 'sky-visual__container__horizon-house-g__roof')
+            .attr('width', 32)
+            .attr('height', 32)
+            .attr('transform', 'translate(0 -16), rotate(45, 20, 20)')
+        this.horizonHouse
+            .append('rect')
+            .attr('class', 'sky-visual__container__horizon-house-g__chimney')
+            .attr('width', 7)
+            .attr('height', 20)
+            .attr('transform', 'translate(28, -20)')
+
         timer((elapsed) => {
             const maxDimension = this.getMaxDimension();
-            const cx = maxDimension / 2;
-            const cy = maxDimension / 2;
+            const cx = maxDimension * 0.5;
+            const cy = maxDimension * this.horizonPositionFraction;
             const angle = (elapsed * 0.001) % 360;
             this.starsContainer.attr('transform', `rotate(${angle}, ${cx}, ${cy})`);
         });
