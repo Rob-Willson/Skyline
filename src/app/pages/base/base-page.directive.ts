47caa83ec5cde7999eca92e2406ae855038c902d
Rob-Willson <ribrob@gmail.com>
2025-03-24 11:47:07 +0000

    use page base directive to handle shared logic

---
+++ src/app/pages/base/base-page.directive.ts
@@ -0,0 +1,55 @@
+import { ChangeDetectorRef, DestroyRef, Directive, EffectRef, ElementRef, inject, OnInit } from '@angular/core';
+import { NavigationService } from '../../services/navigation.service';
+import { NavButton } from '../../shared/types/nav-button.model';
+
+@Directive({})
+export class BasePageDirective implements OnInit {
+
+    public width!: number;
+    public height!: number;
+    protected resizeObserver!: ResizeObserver;
+
+    protected readonly elementRef: ElementRef = inject(ElementRef);
+    protected readonly destroyRef: DestroyRef = inject(DestroyRef);
+    protected readonly navService: NavigationService = inject(NavigationService);
+    protected readonly changeDetectorRef: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+    public ngOnInit(): void {
+        this.initialiseHeader();
+        this.initialiseFooter();
+        this.initialiseResizeObserver();
+    }
+
+    public ngOnDestroy(): void {
+        this.resizeObserver.disconnect();
+    }
+
+    protected initialiseHeader(buttons?: NavButton[] ): void {
+        this.navService.setHeaderButtons(buttons ?? this.getDefaultHeaderButtons());
+    }
+
+    protected initialiseFooter(buttons?: NavButton[]): void {
+        this.navService.setFooterButtons(buttons ?? this.getDefaultFooterButtons());
+    }
+
+    protected initialiseResizeObserver(): void {
+        this.resizeObserver = new ResizeObserver(() => {
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+            this.changeDetectorRef.markForCheck();
+        });
+        this.resizeObserver.observe(this.elementRef.nativeElement);
+    }
+
+    private getDefaultHeaderButtons(): NavButton[] {
+        return [
+            { id: 'about', label: 'About', ariaLabel: 'About', iconName:'help', hideLabel: true, showLabelOnHover: true }
+        ];
+    }
+    
+    private getDefaultFooterButtons(): NavButton[] {
+        return [
+            { id: 'contact', label: 'Contact', ariaLabel: 'Contact', iconName:'email', hideLabel: true, showLabelOnHover: true }
+        ];
+    }
+}
--- src/app/pages/cityscape/cityscape-page.component.ts
+++ src/app/pages/cityscape/cityscape-page.component.ts
@@ -1,10 +1,10 @@
 import { Component, ChangeDetectionStrategy, OnInit, OnDestroy, ElementRef, ChangeDetectorRef, DestroyRef, inject } from '@angular/core';
-import { NavButton } from '../../shared/types/nav-button.model';
 import { NavigationService } from '../../services/navigation.service';
 import { SkyVisualComponent } from "../../organisms/sky-visual/sky-visual.component";
 import { PointMagnitude } from '../../shared/types/point.model';
 import { StarsService } from '../../services/stars.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { BasePageDirective } from '../base/base-page.directive';
 
 @Component({
     selector: 'cityscape-page',
@@ -14,51 +14,20 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     styleUrl: './cityscape-page.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityscapePageComponent implements OnInit, OnDestroy {
-    private readonly headerButtons: NavButton[] = [
-        { id: 'about', label: 'About', ariaLabel: 'About', iconName:'help', hideLabel: true, showLabelOnHover: true }
-    ];
-
-    private readonly footerButtons: NavButton[] = [
-        { id: 'contact', label: 'Contact', ariaLabel: 'Contact', iconName:'email', hideLabel: true, showLabelOnHover: true }
-    ];
+export class CityscapePageComponent extends BasePageDirective implements OnInit, OnDestroy {
 
     public data!: PointMagnitude[];
-    public width!: number;
-    public height!: number;
-
-    private resizeObserver!: ResizeObserver;
-    private readonly destroyRef: DestroyRef = inject(DestroyRef);
 
     public constructor(
-        private readonly elementRef: ElementRef,
-        private readonly navService: NavigationService,
-        private readonly changeDetectorRef: ChangeDetectorRef,
         private readonly starsService: StarsService,
-    ) {}
+    ) {
+        super();
+    }
 
-    public ngOnInit(): void {
-        this.initialiseHeaderFooter();
-        this.initialiseResizeObserver();
+    public override ngOnInit(): void {
+        super.ngOnInit();
+
         this.getStarData();
-    }
-
-    public ngOnDestroy(): void {
-        this.resizeObserver.disconnect();
-    }
-
-    private initialiseHeaderFooter(): void {
-        this.navService.setHeaderButtons(this.headerButtons);
-        this.navService.setFooterButtons(this.footerButtons);
-    }
-
-    private initialiseResizeObserver(): void {
-        this.resizeObserver = new ResizeObserver(() => {
-            this.width = window.innerWidth;
-            this.height = window.innerHeight;
-            this.changeDetectorRef.markForCheck();
-        });
-        this.resizeObserver.observe(this.elementRef.nativeElement);
     }
 
     private getStarData(): void {
