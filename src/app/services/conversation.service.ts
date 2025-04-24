59cc1acaed2be51cfe8000280e4ab97f1125c8aa
Rob-Willson <ribrob@gmail.com>
2025-04-24 10:03:53 +0100

    tweak conversation display

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -42,14 +42,15 @@ export class ConversationService {
             id: `how_are_you`,
             cases: [
                 { input: `good`, output: `Wonderful! How can I help you today?`, goToStateId: `main` } as ConversationCase,
-                { input: `bad`, output: `I'm sorry to hear that. Perhaps there's something I can help with.`, goToStateId: `main` } as ConversationCase,
+                { input: `bad`, output: `I'm sorry to hear that. I urge you to reach out to friends and family, or seek professional support. You should probably do that now, instead of playing around on this website.`, goToStateId: `0` } as ConversationCase,
             ],
         } as ConversationState,
         {
             id: `main`,
             cases: [
                 { input: `who are you?`, output: `I'm a manifestation of Rob's underemployment.` } as ConversationCase,
-                { input: `goodbye`, output: `Goodbye for now.`, goToStateId: `0` } as ConversationCase,
+                { input: `stars`, output: `Aren't they beautiful? You can change the number of stars if you wish. Well... not just by wishing, there's a settings menu, of course.` } as ConversationCase,
+                { input: `goodbye`, output: `Goodbye for now!`, goToStateId: `0` } as ConversationCase,
             ],
         } as ConversationState,
     ];
